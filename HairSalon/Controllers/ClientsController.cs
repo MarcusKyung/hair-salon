1895c5e086d9144aa9b90534ad1b992472044042
MarcusKyung <Kyungmj@gmail.com>
2023-05-19 09:53:36 -0700

    add controller using directives and index route

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HairSalon.Controllers
+{
+}
