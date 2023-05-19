1b9604a504805e575ee444c4760dd525c45e8516
MarcusKyung <Kyungmj@gmail.com>
2023-05-19 10:47:25 -0700

    add clientcontroller index() and index view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -8,4 +8,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Controllers
 {
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                                  .Include(client => client.Stylist)
+                                  .OrderBy(client => client.Stylist.StylistName) //added code to order by ascending
+                                  .ToList();
+      ViewBag.PageTitle = "View All Clients";                     
+      return View(model);
+    }
+  }
 }
